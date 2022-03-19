94a08733326eb1cc2fcd01912ffaf590f039a279
shah-jalal1 <shah.jalal0894@gmail.com>
2022-03-19 12:28:36 +0600

    api data

--- src/__test__/Home.test.tsx
+++ src/__test__/Home.test.tsx
@@ -1,11 +1,21 @@
 import React from "react";
 import { render, screen } from '@testing-library/react';
 import HomePage from "../components/HomePage";
+import { getAllData } from './../AllApi/dataApi';
 
 describe("Home", ()=> {
+    
+    test("api data", async () => {
+        return await getAllData(0).then((data) => {
+          expect(data).toBeDefined();
+        });
+      });
+
     test("Home componenet", ()=> {
         render(<HomePage />)
         const element = screen.getByTestId("homePage");
         expect(element).toBeInTheDocument();
     })
+
+   
 })
