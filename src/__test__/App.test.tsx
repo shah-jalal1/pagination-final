94f231c58095146c16f028c9ebf2fe5910382653
shah-jalal1 <shah.jalal0894@gmail.com>
2022-03-19 23:36:34 +0600

    last

--- src/__test__/App.test.tsx
+++ src/__test__/App.test.tsx
@@ -1,4 +1,4 @@
-import react from "react";
+import React from "react";
 import { render, screen } from '@testing-library/react';
 import App from './../App';
 import { Router } from "react-router-dom";
