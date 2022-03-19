5ae44132175eb5485239c444b6328cbd24a74246
shah-jalal1 <shah.jalal0894@gmail.com>
2022-03-19 12:18:52 +0600

    details test

--- src/__test__/App.test.tsx
+++ src/__test__/App.test.tsx
@@ -1,6 +1,8 @@
 import react from "react";
 import { render, screen } from '@testing-library/react';
 import App from './../App';
+import { Router } from "react-router-dom";
+import { createMemoryHistory } from "history";
 
 describe("app", () => {
     test('Render app component', () => {
@@ -8,4 +10,15 @@ describe("app", () => {
         const app = screen.getByTestId("app");
         expect(app).toBeInTheDocument();
     });
+
+    test("details page", () => {
+        const history = createMemoryHistory({ initialEntries: ["/post-details"] });
+        render(
+          <Router history={history}>
+            <App />
+          </Router>
+        );
+    
+        expect(history.location.pathname).toBe("/post-details");
+      });
 })
