c38e807751bb984f4fb872e860f03f03ff4f9bee
shah-jalal1 <shah.jalal0894@gmail.com>
2022-03-19 23:59:45 +0600

    last

--- src/__test__/PostDetails.test.tsx
+++ src/__test__/PostDetails.test.tsx
@@ -5,28 +5,28 @@ import { render, screen } from '@testing-library/react';
 import { createMemoryHistory } from "history";
 
 describe("details componenet", () => {
-  // test("post details", () => {
-  //   const history = createMemoryHistory();
-  //   render(
-  //     <Router history={history}>
-  //       <PostDetails />
-  //     </Router>
-  //   );
-  //   const linkElement = screen.getByTestId("detailsPost");
-  //   expect(linkElement).toBeInTheDocument();
-  // });
+  test("post detailas", () => {
+    const history = createMemoryHistory();
+    render(
+      <Router history={history}>
+        <PostDetails />
+      </Router>
+    );
+    const linkElement = screen.getByTestId("detailsPost");
+    expect(linkElement).toBeInTheDocument();
+  });
 
   test('Render details component', async () => {
 
     const history = createMemoryHistory();
-    history.push("/details", {title: "", url: "", created_at: Date(), author: ""});
+    history.push("/post-details", {title: "", url: "", created_at: Date(), author: ""});
 
    render(
         <Router history={history}>
             {/* <Details/> */}
             <Switch>
                 <Route exact component={PostDetails}/>
-                <Route path="/details" render={() => <div>created</div>} />
+                <Route path="/post-details" render={() => <div>created</div>} />
             </Switch>
         </Router>
     );
