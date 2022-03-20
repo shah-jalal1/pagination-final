b293211666c4315d78e7668a95e79848ac8b05eb
shah-jalal1 <shah.jalal0894@gmail.com>
2022-03-20 23:51:12 +0600

    without error

--- src/AllApi/dataApi.ts
+++ src/AllApi/dataApi.ts
@@ -1,12 +1,12 @@
 import axios from "axios";
 
 export const getAllData = async(pageNumber: Number) => {
-    // const res = await fetch(
-    //     `https://hn.algolia.com/api/v1/search_by_date?tags=story&page=${pageNumber}`
-    // )
-    const {data} = await axios.get(
+    const res = await fetch(
         `https://hn.algolia.com/api/v1/search_by_date?tags=story&page=${pageNumber}`
     )
-    // const data = await res.json();
+    // const {data} = await axios.get(
+    //     `https://hn.algolia.com/api/v1/search_by_date?tags=story&page=${pageNumber}`
+    // )
+    const data = await res.json();
     return data;
 }
--- src/__test__/HomePage.test.tsx
+++ src/__test__/HomePage.test.tsx
@@ -29,6 +29,11 @@ describe("Home", () => {
     });
   });
 
+    test('api test', async()=> {
+    const data = await getAllData(1);
+    expect(data).toBeDefined()
+});
+
   test("if pagination exist should check", () => {
     render(<HomePage />)
     const element = screen.getByTestId("homePage");
@@ -55,6 +60,54 @@ describe("Home", () => {
   })
 
 
-
-
 })
+
+
+
+// import React from "react";
+// import { render, screen } from '@testing-library/react';
+// import HomePage from "../components/HomePage";
+// import { getAllData } from './../AllApi/dataApi';
+
+
+// describe("Home", () => {
+
+
+//   // test("api data", async () => {
+//   //   return await getAllData(0).then((data) => {
+//   //     expect(data).toBeDefined();
+//   //   });
+//   // });
+
+//   test('api test', async()=> {
+//     const data = await getAllData(1);
+//     expect(data).toBeDefined()
+// });
+
+//   test("if pagination exist should check", () => {
+//     render(<HomePage />)
+//     const element = screen.getByTestId("homePage");
+//     expect(element).toBeInTheDocument();
+//   })
+
+//   test("is loading working", () => {
+//     render(<HomePage />)
+//     const element = screen.getByTestId("loading");
+//     expect(element).toBeInTheDocument();
+//   })
+
+//   test("loading text", () => {
+//     render(<HomePage />)
+//     const element = screen.getByTestId("loadingText");
+//     expect(element).toBeInTheDocument();
+//   })
+
+
+//   test("Home componenet", () => {
+//     render(<HomePage />)
+//     const element = screen.getByTestId("homePage");
+//     expect(element).toBeInTheDocument();
+//   })
+
+
+// })
